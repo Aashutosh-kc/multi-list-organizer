3f6d6df135d3fcbf3aea2637c07b4fd04f8c7d3f
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-05-16 12:35:47 +0545

    feat: add enter key functionality for list items too

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -8,6 +8,11 @@ function ListItem({list,addItem,color,removeListItem}){
 		addItem(list.id ,{ id: Date.now(), value: input} );
 		setInput('');
 	}
+	function handleKeyDown(e){
+		if (e.key == "Enter"){
+			handleAdd();
+		}
+	}
 	return(
 	<div className="add-activity" style={{ '--card-accent': color}}>
 		<div className="title">
@@ -15,7 +20,7 @@ function ListItem({list,addItem,color,removeListItem}){
 		</div>
 		<div className="user-input">
 		<input type="text" value={input} placeholder="e.g. Vegetables"
-		onChange={(e) => setInput(e.target.value)} />
+		onChange={(e) => setInput(e.target.value)} onKeyDown={handleKeyDown}/>
 		<button onClick={handleAdd} className='add-btn' >Add</button>
 		</div>
 		<ul>
